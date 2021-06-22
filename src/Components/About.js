c13c6513a1eae2c97b342a50a661f860c09fa027
ansorudin <ahmadansorudin@gmail.com>
2021-06-22 13:51:53 +0700

    first commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
+      var name = this.props.data.fullName;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
@@ -20,12 +20,12 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Ansorudin Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>{bio}</p>
+            <p>Fast learner and have a strong understanding about HTML, CSS, JavaScript and some framework in JavaScript like React JS, React Native, Node JS, Express. Moreover i’m have understanding about database like MySql, MongoDB and Redis. I’m passionated in Front End Development well it’s Web or Mobile and Backend Development, but does not rule out the possibility other role as long as it's still relevant from my background.</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
@@ -40,7 +40,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href='https://drive.google.com/file/d/1ZysDk_zQiABRrbFx6gd_T_ZctZF7wb61/view?usp=sharing' className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
